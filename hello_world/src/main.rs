137d934f33cd16f15365a941d07a179580210b16
@imrank03 <immu0396@gmail.com>
2022-08-26 23:42:56 +0530

    traditional Hello World program added

---
+++ hello_world/src/main.rs
@@ -0,0 +1,13 @@
+// This is a comment, and is ignored by the compiler
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+
+// This code is editable, feel free to hack it!
+
+// This is the main function
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+} // This is the closing bracket for the main function and the program
